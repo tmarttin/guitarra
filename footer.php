e33ae80afb3d6d6a4863e0fed0b61d4dbc43ae55
tmarttin <163464544+tmarttin@users.noreply.github.com>
2024-03-14 17:43:06 -0300

    Add files via upload
    Alterações efetuadas no dia 14 de março de 2024

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<!--Container que abriga o rodapé da página-->
+<div class="container-fluid text-center bg-black text-light fixed-bottom">
+    <p>Sistema web desenvolvido por Tiago Martins</p>
+</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4Guitars</title>
+    <!-- Ultima versao compilada CSS-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<!--CND do google fonts-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Libre+Barcode+39+Text&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- container que abriga o logotipo da empresa-->
+    <div class="container-fluid text-light text-center text-black mt-3 mb-3"> <!-- caracteristicas dentro do container-->
+        <h1 style="font-family:Bebas Neue">4GUITARS
+            <img src="img/Guitarlogo2.png" width="100">
+        </h1> 
+    </div>
+
+    <!--- Barra de navegação do sistama -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php" title="Ir para pagina inicial">Página Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Carrinho</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Contaiter que abriga o conteudo da pagina-->
+    <div class= "container text-center  mt-5 mb-5 ">
